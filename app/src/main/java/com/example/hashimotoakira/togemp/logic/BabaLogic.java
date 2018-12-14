81fcaf5fde709edc1e52b489fe91c43e3cd05263
野口雄希 <y.n.10191@gmail.com>
2018-12-14 22:24:18 +0900

    プレイヤーの順番を保存するメソッドを追加

--- app/src/main/java/com/example/hashimotoakira/togemp/logic/BabaLogic.java
+++ app/src/main/java/com/example/hashimotoakira/togemp/logic/BabaLogic.java
@@ -1,11 +1,25 @@
 package com.example.hashimotoakira.togemp.logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BabaLogic {
     List<Player> playerList;
 
+    public BabaLogic() {
+        this.playerList = new ArrayList<>();
+    }
+
     public void addPlayer(String id) {
         playerList.add(new Player(id));
+    }
+
+    public void addPlayerPosition(String id, int position) {
+        for (int i = 0; i < playerList.size(); i++) {
+            Player player = playerList.get(i);
+            if (id.equals(player.getId())){
+                player.setPosition(position);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/hashimotoakira/togemp/logic/Player.java
+++ app/src/main/java/com/example/hashimotoakira/togemp/logic/Player.java
@@ -2,15 +2,34 @@ package com.example.hashimotoakira.togemp.logic;
 
 public class Player {
 
+    public String id;
+    public int position;
+    public int cardCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
     public void setPosition(int position) {
         this.position = position;
     }
 
-    public int position;
+    public int getCardCount() {
+        return cardCount;
+    }
 
-    public String id;
+    public void setCardCount(int cardCount) {
+        this.cardCount = cardCount;
+    }
 
-    public int cardCount;
 
     Player(String id) {
         this.id = id;
