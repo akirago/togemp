c9ae2dcd90365acc2af00812ed6d3e311e9231ec
Yoshio Kajikuri <pdgwm129@gmail.com>
2018-12-15 15:47:58 +0900

    messageのやりとりクラス作成

--- app/src/main/java/com/example/hashimotoakira/togemp/logic/Card.java
+++ app/src/main/java/com/example/hashimotoakira/togemp/logic/Card.java
@@ -4,6 +4,10 @@ public class Card {
     public  String suit;
     public  int number;
 
+    //  jacksonのライブラリーを使うため空のコンストラクタが必須
+    public Card() {
+    }
+
     public Card(String suit, int number) {
         this.suit = suit;
         this.number = number;
